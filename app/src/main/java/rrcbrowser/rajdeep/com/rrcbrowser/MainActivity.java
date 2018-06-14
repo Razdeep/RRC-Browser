86f3360f94821a1752281f8c66545a70019bfb67
razdeep <rrajdeeproychowdhury@gmail.com>
2018-06-14 13:30:50 +0530

    Android Project initiated

---
+++ app/src/main/java/rrcbrowser/rajdeep/com/rrcbrowser/MainActivity.java
@@ -0,0 +1,13 @@
+package rrcbrowser.rajdeep.com.rrcbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
